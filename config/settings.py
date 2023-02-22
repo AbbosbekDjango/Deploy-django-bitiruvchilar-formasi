64d674acb41279a2009950fb3284fd87c6ed368c
AbbosbekDjango <126000563+AbbosbekDjango@users.noreply.github.com>
2023-02-22 07:25:04 +0500

    ali

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qypm#np8mx%izx%tsrswo9ds@e+y69fq&zqq$$q5qs&itm+w##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
